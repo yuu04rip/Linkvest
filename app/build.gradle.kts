8f74d79fefd97f9210b009deba575707ad36cbc3
yuu04rip <99676204+yuu04rip@users.noreply.github.com>
2025-08-01 14:19:25 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "11"
@@ -56,4 +56,25 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    val composeBom = platform("androidx.compose:compose-bom:2025.05.00")
+    implementation("androidx.compose:compose-bom:2025.07.00")
+    androidTestImplementation("androidx.compose:compose-bom:2025.07.00")
+
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+// Optional icons
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+
+// Optional adaptive layout
+    implementation("androidx.compose.material3.adaptive:adaptive")
+
+// Integration with Activity/ViewModel/LiveData
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+    implementation("androidx.compose.runtime:runtime-livedata")
 }
