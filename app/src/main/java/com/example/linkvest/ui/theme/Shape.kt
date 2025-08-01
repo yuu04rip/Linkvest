2ebff6b297a4383816ae2f1cde408af79590912a
yuu04rip <99676204+yuu04rip@users.noreply.github.com>
2025-08-01 14:53:50 +0200

    New file

---
+++ app/src/main/java/com/example/linkvest/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.example.linkvest.ui.theme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val AppShapes = Shapes(
+    small = RoundedCornerShape(16.dp),
+    medium = RoundedCornerShape(24.dp),
+    large = RoundedCornerShape(32.dp)
+)
---
+++ app/src/main/java/com/example/linkvest/ui/theme/screens/RegisterScreen.kt
@@ -0,0 +1,1 @@
+package com.example.linkvest.ui.theme.screens
